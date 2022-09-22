c45721e0d86906d17a5ad8a0d034bc0b57fc62c6
Carolina <carolinazapatavidal@gmail.com>
2022-09-22 20:50:59 -0300

    categories select and getData file

---
+++ src/getData.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+// let headers = {
+//   Authorization: `Bearer ${localStorage.getItem('token')}`
+// }
+
+// axios
+//   .get('https://apiburgerqueenv1.herokuapp.com/', { headers })
+//   .then((response)=> {
+//     console.log(response.data)
+//   })
+//   .catch((error)=> {
+//     console.log(error)
+//   })
