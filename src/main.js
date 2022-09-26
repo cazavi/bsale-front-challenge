2f6ef228e99a48966db33a747b8b6aab0124aa7f
Carolina <carolinazapatavidal@gmail.com>
2022-09-26 13:49:17 -0300

    products ready

--- src/main.js
+++ src/main.js
@@ -1,80 +1,78 @@
-const getCategories =
-axios
-  .get('https://bsaleapichallenge.herokuapp.com')
-  .then((response)=> {
+const getCategories = axios
+  .get("https://bsaleapichallenge.herokuapp.com")
+  .then((response) => {
     const cData = response.data.result;
-    
   })
-  .catch((error)=> {
-    console.log(error)
-  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-  const getProducts =
-  axios
-  .get('https://bsaleapichallenge.herokuapp.com/product')
-  .then((response)=> {
+const getProducts = axios
+  .get("https://bsaleapichallenge.herokuapp.com/product")
+  .then((response) => {
     const pData = response.data.result;
-    filterSearch(pData);  })
-  .catch((error)=> {
-    console.log(error)
+    order(pData);
+    filterSearch(pData);
   })
+  .catch((error) => {
+    console.log(error);
+  });
 
-// const products = [
-//   {name:'Plátanos', price:500},
-//   {name:'Frutilla', price:600},
-//   {name:'Sandia', price:700},
-//   {name:'Melon', price:800},
-//   {name:'Pera', price:900},
-//   {name:'Manzana', price:1000}
-// ]
-
-const search = document.querySelector('#searchInput');
-// const btnSearch = document.querySelector('#searchButton');
-const result = document.querySelector('#result');
-
-const filter= document.getElementById('#filter')
-
-
-
-const filterSearch = (cdata) => {
-  result.innerHTML = '';
-  const text = search.value.toLowerCase();
-  for (let product of cdata) {
-    let name = product.name.toLowerCase();
-    if (name.indexOf(text) !== -1) {
-    
-      // document.querySelector('productImg').setAttribute('src',product.url_image)
-      
-      result.innerHTML += `
-
-
-      <h5 class="productName">${product.name}</h5>
-      <div class="prices">
-      <span class="productPrice ">$${product.price}</span>
-      <span class="productPrice ">$${product.price-product.discount}</span>
-      <span class="productPrice ">$${product.discount}</span>
-      <a class="add">Agregar</a>
-      </div>
-
-
-      `
-    }
-    const image = document.createElement('img')
-    image.classList.add('productImg')
-    image.src  = product.url_image
-    document.querySelector('#result').appendChild(image)
-  }
-  if (result.innerHTML === '') {
-    result.innerHTML += `
-    <li>Producto no encontrado...</li>
-    `
-  }
+function order(data) {
+  data.sort((a, b) => {
+    return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1;
+  });
+  return data;
 }
 
-// btnSearch.addEventListener('click', filterSearch);
-search.addEventListener('keyup', filterSearch);
-filterSearch();
+const search = document.querySelector("#searchInput");
 
-      // <li>${product.name}-Precio:${product.price}</li>
-      // <img class="p-8 rounded-t-lg" src="/docs/images/products/apple-watch.png" alt="product image">
+const result = document.querySelector("#result");
 
+const filter = document.getElementById("#filter");
+
+const filterSearch = (cdata) => {
+  for (let product of cdata) {
+    const card = document.createElement("div");
+    card.classList.add("cardDiv");
+
+    const productName = document.createElement("h5");
+    productName.classList.add("productName");
+    productName.textContent = product.name;
+    card.appendChild(productName);
+
+    const image = document.createElement("img");
+    image.classList.add("productImg");
+    image.src = product.url_image;
+    card.appendChild(image);
+
+    const discountPrice = document.createElement("p");
+    discountPrice.classList.add("discountPrice");
+    discountPrice.textContent = `$ ${product.price - product.discount}`;
+    card.appendChild(discountPrice);
+
+    const discount = document.createElement("p");
+    discount.classList.add("discount");
+    discount.textContent = `-$ ${product.discount}`;
+    card.appendChild(discount);
+
+    const price = document.createElement("p");
+    price.classList.add("prices");
+    price.textContent = `$ ${product.price}`;
+    card.appendChild(price);
+    
+  result.appendChild(card);
+  }
+  document.addEventListener("keyup", (e) => {
+    if (e.target.matches("#searchInput")) {
+      if (e.key === "Escape") e.target.value = "";
+
+      document.querySelectorAll(".cardDiv").forEach((product) => {
+        product.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+          ? product.classList.remove("filtro")
+          : product.classList.add("filtro");
+      });
+    }
+  });
+  filterSearch();
+};
